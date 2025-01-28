a7bb8c872f46589317ad6e733f663eeaa5e1625e
Dillon May <dtamy2019@yahoo.com>
2025-01-28 14:35:23 -0500

    updates

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.dillyenterprise.ironvault"},{"l":"com.dillyenterprise.ironvault.exceptions"}];updateSearchResults();
